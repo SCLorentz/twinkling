01feaa782a611a26992d81184403b19e46c47944
Felipe S.C. Lorentz <113468900+SCLorentz@users.noreply.github.com>
2024-10-21 00:39:45 +0000

    better html handling

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,49 @@
 use std::str::Chars;
 
 #[derive(Debug)]
-struct Paragraph {
-    content: String,
+enum HtmlNode {
+    Text(String),
+    Element {
+        #[allow(unused)]
+        tag: String,
+        #[allow(unused)]
+        attributes: Option<Vec<(String, String)>>,
+        children: Vec<HtmlNode>,
+    },
+}
+
+fn expect_value(r#char: char, mut value: Chars<'_>) -> Result<Chars<'_>, String>
+{
+    if value.next() == Some(r#char) {
+        Ok(value)
+    } else {
+        Err(format!("missing args: `{}`", r#char).to_string())
+    }
 }
 
 fn begin_tag_struct(mut value: Chars<'_>, tag: char) -> Result<Chars<'_>, String>
 {
-    if value.next() == Some('<') && value.next() == Some(tag) && value.next() == Some('>')
-    {
-        return Ok(value)
-    }
-    else
-    {
-        return Err("tag not found!".to_string())
-    }
+    value = expect_value('<', value)?;
+    value = expect_value(tag, value)?;
+    value = expect_value('>', value)?;
+    Ok(value)
 }
 
 fn end_tag_struct(mut value: Chars<'_>, tag: char) -> Result<Chars<'_>, String>
 {
-    if value.next() == Some('<') && value.next() == Some('/') && value.next() == Some(tag) && value.next() == Some('>') {
-        return Ok(value)
-    } else {
-        return Err("end of tag not found!".to_string())
-    }
+    value = expect_value('<', value)?;
+    value = expect_value('/', value)?;
+    value = expect_value(tag, value)?;
+    value = expect_value('>', value)?;
+    Ok(value)
 }
 
-fn parse_p_tag(input: &str) -> Result<(Paragraph, &str), String>
+fn parse_p_tag(input: &str) -> Result<(HtmlNode, &str), String>
 {
     let (mut chars, mut content) = (input.chars(), String::new());
     //
     chars = begin_tag_struct(chars.to_owned(), 'p')?;
-
+    
     while let Some(char) = chars.next()
     {
         let val = end_tag_struct(chars.to_owned(), 'p');
@@ -44,7 +56,26 @@ fn parse_p_tag(input: &str) -> Result<(Paragraph, &str), String>
         content.push_str(&char.to_string());
     }
 
-    Ok((Paragraph { content: content.clone() }, chars.as_str()))
+    let return_value = HtmlNode::Element {
+        tag: String::from("p"),
+        attributes: None,
+        children: vec![HtmlNode::Text(content)],
+    };
+
+    return Ok((return_value, chars.as_str()))
+}
+
+fn extract_text(node: &HtmlNode) -> String {
+    match node {
+        HtmlNode::Text(text) => text.to_owned(),
+        HtmlNode::Element { children, .. } => {
+            let mut result = String::new();
+            for child in children {
+                result += &extract_text(child);
+            }
+            result
+        }
+    }
 }
 
 fn main()
@@ -53,7 +84,7 @@ fn main()
     let result = parse_p_tag(html);
 
     match result {
-        Ok((paragraph, m)) => println!("{:#?} {:?}", paragraph.content, m),
+        Ok((html, m)) => println!("text: {:#?}, tag: unknow, remain: {:?}", extract_text(&html), m),
         Err(error) => println!("Erro: {}", error),
     }
 }
